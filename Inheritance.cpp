a1a21e81b8a939c3578d5482abd5a670ef43c27b
Ashish Kumar Adarsh <ashishadarsh319@gmail.com>
2023-08-18 17:55:54 +0530

    multiInheritance

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class Human
 {
-private:
+public:
     int age;
     int weight;
     int height;
@@ -23,10 +23,7 @@ public:
     {
         height = newHeight;
     }
-    int getAge()
-    {
-        return this->age;
-    }
+
     int getWeight()
     {
         return this->weight;
@@ -37,7 +34,7 @@ public:
     }
 };
 
-class Male : public Human
+class Male : private Human
 {
 public:
     int bmi;
@@ -46,6 +43,14 @@ public:
     int getBmi()
     {
         return this->bmi;
+    }
+    int getAge()
+    {
+        return this->age;
+    }
+    void setAge(int newAge)
+    {
+        age = newAge;
     }
     void setBmi(int newBmi)
     {
@@ -58,12 +63,12 @@ int main()
     Male Bhaskar;
 
     Bhaskar.setAge(20);
-    Bhaskar.setWeight(60);
-    Bhaskar.setHeight(5.8);
+    // Bhaskar.setWeight(60);
+    // Bhaskar.setHeight(5.8);
     Bhaskar.setBmi(20);
 
     cout << "Age: " << Bhaskar.getAge() << endl;
-    cout << "Weight: " << Bhaskar.getWeight() << endl;
-    cout << "Height: " << Bhaskar.getHeight() << endl;
+    // cout << "Weight: " << Bhaskar.getWeight() << endl;
+    // cout << "Height: " << Bhaskar.getHeight() << endl;
     cout << "BMI: " << Bhaskar.getBmi() << endl;
 }
---
+++ Inheritance2single.cpp
@@ -0,0 +1,108 @@
+// making single inheritance
+#include <iostream>
+using namespace std;
+
+class Animal
+{
+public:
+    int age;
+    int weight;
+
+public:
+    void setAge(int newAge)
+    {
+        age = newAge;
+    }
+
+    void setWeight(int newWeight)
+    {
+        weight = newWeight;
+    }
+
+    int getAge()
+    {
+        return this->age;
+    }
+    int getWeight()
+    {
+        return this->weight;
+    }
+};
+
+class Dog : public Animal
+{
+public:
+    string breed;
+    string color;
+
+public:
+    void setBreed(string newBreed)
+    {
+        breed = newBreed;
+    }
+
+    void setColor(string newColor)
+    {
+        color = newColor;
+    }
+
+    string getBreed()
+    {
+        return this->breed;
+    }
+    string getColor()
+    {
+        return this->color;
+    }
+    int getage()
+    {
+        return this->age;
+    }
+};
+
+class GermanShepherd : public Dog
+{
+public:
+    string name;
+    string owner;
+
+public:
+    void setName(string newName)
+    {
+        name = newName;
+    }
+
+    void setOwner(string newOwner)
+    {
+        owner = newOwner;
+    }
+
+    string getName()
+    {
+        return this->name;
+    }
+    string getOwner()
+    {
+        return this->owner;
+    }
+};
+
+int main()
+{
+    GermanShepherd gs1;
+    gs1.setAge(10);
+    gs1.setWeight(20);
+    gs1.setBreed("German Shepherd");
+    gs1.setColor("Black");
+    gs1.setName("Tommy");
+    gs1.setOwner("Rahul");
+
+    cout << "Age: " << gs1.getage() << endl;
+    cout << "Weight: " << gs1.getWeight() << endl;
+    cout << "Breed: " << gs1.getBreed() << endl;
+    cout << "Color: " << gs1.getColor() << endl;
+    cout << "Name: " << gs1.getName() << endl;
+    cout << "Owner: " << gs1.getOwner() << endl;
+
+    return 0;
+}
